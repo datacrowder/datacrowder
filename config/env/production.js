eafa6cdb016cbf6a1197c611a56ee3ee53dbcf4b
Vlad Nastasiu <vlad@nastasiu.com>
2015-03-23 18:26:29 +0200

    heroku db

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: {
-		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/datacrowder',
+		uri: 'mongodb://ds037551.mongolab.com:37551/heroku_app35162905',
 		options: {
 			user: '',
 			pass: ''
