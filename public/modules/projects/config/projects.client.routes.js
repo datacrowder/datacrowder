c2b62f42c0d705ef5706e71bc325f478d5673e0c
filipmanisor <filip.manisor@gmail.com>
2015-04-16 12:46:04 +0300

    rename views

--- public/modules/projects/config/projects.client.routes.js
+++ public/modules/projects/config/projects.client.routes.js
@@ -9,25 +9,25 @@ angular.module('projects').config(['$stateProvider',
 			url: '/feed',
 			templateUrl: 'modules/projects/views/feed.client.view.html'
 		}).
-		state('listProjects', {
+		state('research', {
 			url: '/projects',
-			templateUrl: 'modules/projects/views/list-projects.client.view.html'
+			templateUrl: 'modules/projects/views/research.client.view.html'
 		}).
 		state('createProject', {
 			url: '/projects/create',
-			templateUrl: 'modules/projects/views/create-project.client.view.html'
+			templateUrl: 'modules/projects/views/create.client.view.html'
 		}).
 		state('viewProject', {
 			url: '/projects/:projectId',
-			templateUrl: 'modules/projects/views/view-project.client.view.html'
+			templateUrl: 'modules/projects/views/view.client.view.html'
 		}).
 		state('editProject', {
 			url: '/projects/:projectId/edit',
-			templateUrl: 'modules/projects/views/edit-project.client.view.html'
+			templateUrl: 'modules/projects/views/edit.client.view.html'
 		}).
 		state('answer', {
 			url: '/projects/:projectId/answer',
-			templateUrl: 'modules/projects/views/answer.client.view.html'
+			templateUrl: 'modules/projects/views/contribute.client.view.html'
 		}).
 		state('results', {
 			url: '/projects/:projectId/results',
