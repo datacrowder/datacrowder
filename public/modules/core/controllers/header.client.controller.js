ba3128a018d6d5b3da75d160dd6de07fccee3b03
filipmanisor <filip.manisor@gmail.com>
2015-04-17 15:45:01 +0300

    change feed reload in case of search

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$location', 'Authentication', 'Menus', 'Projects',
-	function($scope, $location, Authentication, Menus, Projects) {
+angular.module('core').controller('HeaderController', ['$scope', '$window', '$location', 'Authentication', 'Menus', 'Projects',
+	function($scope, $window, $location, Authentication, Menus, Projects) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -12,7 +12,14 @@ angular.module('core').controller('HeaderController', ['$scope', '$location', 'A
 
 		// Go to the feed with a search query
 		$scope.search = function() {
-			$location.path('feed').search({q: $scope.query});
+			if ( $window.location.href.indexOf('feed') > -1 ) {
+				// In case currently the window location is the feed page, reload it
+				$window.location.replace('http://localhost:3000/#!/feed?q=' + $scope.query);
+				$window.location.reload(true);
+			}
+			else {
+				$window.location.replace('http://localhost:3000/#!/feed?q=' + $scope.query);
+			}
 		};
 
 		// Collapsing the menu after navigation
