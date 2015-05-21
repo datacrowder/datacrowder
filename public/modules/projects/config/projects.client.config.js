b6462e5f44cca5859a2a6097d0b80e3f2dfcef7f
filipmanisor <filip.manisor@gmail.com>
2015-05-22 00:08:24 +0300

    change header

--- public/modules/projects/config/projects.client.config.js
+++ public/modules/projects/config/projects.client.config.js
@@ -4,9 +4,9 @@
 angular.module('projects').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Feed', 'feed', 'item', 'feed', true);
+		/*Menus.addMenuItem('topbar', 'Feed', 'feed', 'item', 'feed', true);
 		Menus.addMenuItem('topbar', 'Projects', 'projects', 'dropdown', '/projects(/create)?', false);
 		Menus.addSubMenuItem('topbar', 'projects', 'List Projects', 'projects', false);
-		Menus.addSubMenuItem('topbar', 'projects', 'New Project', 'projects/create', false);
+		Menus.addSubMenuItem('topbar', 'projects', 'New Project', 'projects/create', false);*/
 	}
 ]);
